b7970361e5a2ee61257ac717d8916518eac9eaa8
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-03-30 16:16:20 -0600

    order page

--- scripts/fruits.js
+++ scripts/fruits.js
@@ -5,51 +5,21 @@ fetch(url)
 .then(response => response.json())
 .then(data => {
     const fruits = data;
-    console.log(fruits);
 
-
-        let fruitList = document.getElementById("fruit-selection");
-
-
-
-
-        // create a select element
-        const select = document.createElement("select");
+        const fruitSelect1 = document.getElementById("fruit1");
+        const fruitSelect2 = document.getElementById("fruit2");
+        const fruitSelect3 = document.getElementById("fruit3");
 
         // loop through the fruit names array and create an option element for each fruit
         fruits.forEach((fruit) => {
 
-                // create an input element
-                const input = document.createElement("input");
-                // set the type attribute to checkbox
-                input.setAttribute("type", "checkbox");
-                // set the id attribute to "banana"
-                input.setAttribute("id", fruit.name);
-                // set the name attribute to "fruit[]"
-                input.setAttribute("name", "fruit[]");
-                // set the value attribute to "banana"
-                input.setAttribute("value", fruit.name);
+            let option = document.createElement("option");
+            option.value = fruit.name;
+            option.text = fruit.name;
 
-
-                // create a label element
-                const label = document.createElement("label");
-                // set the for attribute to "banana"
-                label.setAttribute("for", fruit.name);
-                // set the text content of the label to "Banana"
-                label.textContent = fruit.name;
-
-                fruitList.appendChild(input);
-                fruitList.appendChild(label);
-
+            fruitSelect1.appendChild(option.cloneNode(true));
+            fruitSelect2.appendChild(option.cloneNode(true));
+            fruitSelect3.appendChild(option.cloneNode(true));
         });
-
-
-
-
-
-  
-  
-
-
 })
 .catch(error => console.log(error));
